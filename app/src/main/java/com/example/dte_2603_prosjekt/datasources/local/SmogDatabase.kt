a1f9cf359447676380da4ae1bbdd031fddcfe9b6
Thomas Ytterdal <thytterdal@pm.me>
2022-04-28 20:10:27 +0200

    Ryddet

--- app/src/main/java/com/example/dte_2603_prosjekt/datasources/local/SmogDatabase.kt
+++ app/src/main/java/com/example/dte_2603_prosjekt/datasources/local/SmogDatabase.kt
@@ -9,26 +9,4 @@ import com.example.dte_2603_prosjekt.datasources.local.entities.DatabaseStation
 @TypeConverters(Converters::class)
 abstract class SmogDatabase : RoomDatabase() {
     abstract val smogDao: SmogDao
-
-//    companion object {
-//        @Volatile
-//        private var INSTANCE: StationsDatabase? = null
-//        fun getInstance(context: Context): StationsDatabase {
-//            synchronized(this) {
-//                var instance = INSTANCE
-//
-//                if (instance == null) {
-//                    instance = Room.databaseBuilder(
-//                        context.applicationContext,
-//                        StationsDatabase::class.java,
-//                        "stations"
-//
-//                    ).fallbackToDestructiveMigration().build()
-//                    INSTANCE = instance
-//                }
-//                return instance
-//            }
-//        }
-//    }
-
 }

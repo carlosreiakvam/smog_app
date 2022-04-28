839a6c39397fe1a96bdd6f0eb06d46e8fbc5bd51
carlosreiakvam <cre032@uit.no>
2022-04-28 16:46:49 +0200

    Add models and API Service

---
+++ app/src/main/java/com/example/dte_2603_prosjekt/domain/models/Meta.kt
@@ -0,0 +1,19 @@
+package com.example.dte_2603_prosjekt.domain.models
+
+// basert på spørring med url
+// http://10.239.120.166/weatherapi/airqualityforecast/0.1/?lat=60&lon=10&areaclass=grunnkrets
+data class Meta(
+    val reftime: String,
+    val location: Location,
+    val superlocation: Location
+)
+
+data class Location(
+    val name: String,
+    val path: String,
+    val areacode: Int,
+    val longitude: Double,
+    val latitude: Double,
+    val areaclass: String,
+    val superareacode: Int
+)
--- app/src/main/java/com/example/dte_2603_prosjekt/network/AirQualityForecastApiService.kt
+++ app/src/main/java/com/example/dte_2603_prosjekt/network/AirQualityForecastApiService.kt
@@ -1,7 +1,37 @@
-package com.example.dte_2603_prosjekt.network
+package com.carlosreiakvam.metapi.network
+import com.example.dte_2603_prosjekt.domain.models.PositionalData
+import com.example.dte_2603_prosjekt.domain.models.Station
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-interface AirQualityForecastApiService {
+private const val BASE_URL = "http://10.239.120.166/weatherapi/airqualityforecast/0.1/"
 
-//    @GET(stations)
-//    suspend fun getStations():
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface AirQualityApiService {
+    @GET("stations")
+    suspend fun getStations(): List<Station>
+
+    @GET(".")
+    suspend fun getPositionalData(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double
+    ): PositionalData
+}
+
+object AirQualityApi {
+    val retrofitService: AirQualityApiService by lazy {
+        retrofit.create(AirQualityApiService::class.java)
+    }
 }

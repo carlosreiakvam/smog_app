e3d17029f72cd8085cfe7e5b13568fc52882fd50
carlosreiakvam <cre032@uit.no>
2022-04-29 00:07:32 +0200

    Add AboutFragment.kt and fragment_about.xml

---
+++ app/src/main/java/com/example/dte_2603_prosjekt/screens/about/AboutFragment.kt
@@ -0,0 +1,17 @@
+package com.example.dte_2603_prosjekt.screens.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AboutFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}

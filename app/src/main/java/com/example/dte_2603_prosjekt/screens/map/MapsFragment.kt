e457feffb88c20c012ed56e81632eceb90af80c2
carlosreiakvam <cre032@uit.no>
2022-04-29 07:55:31 +0200

    Add binding to MapsFragment.kt

--- app/src/main/java/com/example/dte_2603_prosjekt/screens/map/MapsFragment.kt
+++ app/src/main/java/com/example/dte_2603_prosjekt/screens/map/MapsFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dte_2603_prosjekt.R
+import com.example.dte_2603_prosjekt.databinding.FragmentMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -32,12 +33,15 @@ class MapsFragment : Fragment() {
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, zoom))
     }
 
+    private lateinit var binding: FragmentMapsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        binding = FragmentMapsBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
